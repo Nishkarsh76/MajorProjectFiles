29b239a459bbe643f2c6b3f3482b71b4f2e767fe
Nishkarsh76 <nishkarshgupta76@gmail.com>
2019-12-06 12:46:22 +0530

    Add files via upload

---
+++ AdminViewClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mini_Project.Models
+{
+    public class AdminViewClass
+    {
+        //public events eventlist { get; set; }
+        public int sports_evid { get; set; }
+        public int cul_evid { get; set; }
+        public int event_id { get; set; }
+
+    }
+//    public enum events
+//    {
+//       None=0,
+//       Cultural=1,
+//       Sports=2
+
+//    }
+}
---
+++ MultiTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Mini_Project.Models;
+
+namespace Mini_Project
+{
+    public class MultiTable
+    {
+        public IEnumerable<Sport_tournamentlist>
+            reg1
+        { get; set; }
+
+        public IEnumerable<Cultural_eventlist>
+           reg2
+        { get; set; }
+    }
+}
